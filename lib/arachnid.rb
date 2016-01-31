7babd6363c8a9e36959b35fb39e44a36127ede5c
jcran <jcran@pentestify.com>
2016-01-31 10:54:17 -0800

    allow subdomains to be crawled

--- lib/arachnid.rb
+++ lib/arachnid.rb
@@ -22,7 +22,7 @@ class Arachnid
 
   def crawl(options = {})
     threads = options[:threads] || 1
-    max_urls = options[:max_urls]
+    max_urls = options[:max_urls] || 10
     crawl_filter = options[:filter]
 
     @hydra = Typhoeus::Hydra.new(:max_concurrency => threads)
@@ -33,14 +33,14 @@ class Arachnid
     @global_queue.concat @start_urls
 
     while not @global_queue.empty?
-      puts "ARACHNID DEBUG - global queue: #{@global_queue}" if @debug
+      #puts "ARACHNID DEBUG - global queue: #{@global_queue}" if @debug
+
+      puts "ARACHNID DEBUG - max_urls: #{max_urls}" if @debug
+      puts "ARACHNID DEBUG - global_visited: #{@global_visited.size}" if @debug
+      puts "ARACHNID DEBUG - global_queue: #{@global_queue.size}" if @debug
 
       @global_queue.size.times do
         q = @global_queue.shift
-
-        puts "ARACHNID DEBUG - max_urls: #{max_urls}" if @debug
-        puts "ARACHNID DEBUG - global_visited: #{@global_visited.size}" if @debug
-        puts "ARACHNID DEBUG - global_queue: #{@global_queue.size}" if @debug
 
         if !max_urls.nil? && @global_visited.size >= max_urls
           puts "ARACHNID DEBUG - DONE! VISITED (#{@global_visited.size}) > MAX_URLS (#{max_urls})" if @debug
@@ -50,7 +50,7 @@ class Arachnid
 
         next unless crawl_filter.call(q) if crawl_filter
 
-        puts "ARACHNID DEBUG - Processing link: #{q}" if @debug
+        #puts "ARACHNID DEBUG - Processing link: #{q}" if @debug
         @global_visited.insert(q)
 
         # Set default options
@@ -73,48 +73,51 @@ class Arachnid
         end
 
         request = Typhoeus::Request.new(q, options)
-        request.on_complete do |response|
-          next unless Arachnid.parse_domain(response.effective_url) == @domain
 
-          yield response
+        request.on_complete do |response|
+          #puts "ARACHNID DEBUG - Completed request for: #{response.effective_url}" if @debug
+          #puts "ARACHNID DEBUG - effective domain: #{Arachnid.parse_domain(response.effective_url)}" if @debug
+          # Note that this will match subdomains as well.
+          next unless Arachnid.parse_domain(response.effective_url).include? @domain
+
+          #yield response
 
           # Parse the page and pull out internal links
-          puts "ARACHNID DEBUG - page body: #{response.body}" if @debug
-          puts "ARACHNID DEBUG - processing page links from #{response.effective_url}" if @debug
+          #puts "ARACHNID DEBUG - page body: #{response.body}" if @debug
+          puts "ARACHNID DEBUG - Processing page links from #{response.effective_url}" if @debug
           elements = Nokogiri::HTML.parse(response.body).css('a')
           links = elements.map {|link| link.attribute('href').to_s}.uniq.sort.delete_if {|href| href.empty? }
-          puts "ARACHNID DEBUG - links: #{links}" if @debug
+          #puts "ARACHNID DEBUG - links: #{links}" if @debug
           links.each do |link|
             next if link.match(/^\(|^javascript:|^mailto:|^#|^\s*$|^about:/)
             begin
 
               absolute_link = make_absolute(split_url_at_hash(link), response.effective_url)
 
-              puts "ARACHNID DEBUG - got link: #{link}" if @debug
-              puts "ARACHNID DEBUG - absolute link: #{absolute_link}" if @debug
-              puts "ARACHNID DEBUG - internal link? #{internal_link?(link, response.effective_url)}" if @debug
-              puts "ARACHNID DEBUG - no hash? #{no_hash_in_url?(absolute_link)}" if @debug
-              puts "ARACHNID DEBUG - extension not ignored? #{extension_not_ignored?(absolute_link)}" if @debug
-              puts "ARACHNID DEBUG - visited? #{@global_visited.include?(absolute_link)}" if @debug
+              #puts "ARACHNID DEBUG - got link: #{link}" if @debug
+              #puts "ARACHNID DEBUG - absolute link: #{absolute_link}" if @debug
+              #puts "ARACHNID DEBUG - internal link? #{internal_link?(link, response.effective_url)}" if @debug
+              #puts "ARACHNID DEBUG - no hash? #{no_hash_in_url?(absolute_link)}" if @debug
+              #puts "ARACHNID DEBUG - extension not ignored? #{extension_not_ignored?(absolute_link)}" if @debug
+              #puts "ARACHNID DEBUG - visited? #{@global_visited.include?(absolute_link)}" if @debug
 
               if internal_link?(link, response.effective_url) &&
                 !@global_visited.include?(absolute_link) && no_hash_in_url?(absolute_link) && extension_not_ignored?(link)
-
-                puts "ARACHNID DEBUG - Got one! -> #{absolute_link}" if @debug
-
+                #puts "ARACHNID DEBUG - Got one! -> #{absolute_link}" if @debug
                 unless @global_queue.include?(absolute_link)
-                  puts "ARACHNID DEBUG - Adding to global_queue: #{absolute_link}" if @debug
+                  #puts "ARACHNID DEBUG - Adding to global_queue: #{absolute_link}" if @debug
                   @global_queue << absolute_link
                 end
               end
 
             rescue Addressable::URI::InvalidURIError => e
-              puts "ARACHNID DEBUG - #{e.class}: Ignored link #{link} (#{e.message}) on page #{q}" if @debug
+              #puts "ARACHNID DEBUG - #{e.class}: Ignored link #{link} (#{e.message}) on page #{q}" if @debug
             end
           end
-          puts "ARACHNID DEBUG - @global_queue: #{@global_queue}" if @debug
+          #puts "ARACHNID DEBUG - @global_queue: #{@global_queue}" if @debug
         end
-
+        puts "ARACHNID DEBUG - Global queue size: #{@global_queue.size}" if @debug
+        puts "ARACHNID DEBUG - Global visited queue size: #{@global_visited.size}" if @debug
         @hydra.queue request
       end
 
@@ -143,7 +146,7 @@ class Arachnid
   def internal_link?(url, effective_url)
     absolute_url = make_absolute(url, effective_url)
     parsed_url = Arachnid.parse_domain(absolute_url)
-    @domain == parsed_url
+    parsed_url.include? @domain
   end
 
   def split_url_at_hash(url)
@@ -168,3 +171,7 @@ class Arachnid
   end
 
 end
+
+
+x = Arachnid.new("http://yahoo.com",{:debug => true})
+x.crawl
